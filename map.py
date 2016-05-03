ef601fc6f6b401c73253279b7e8a6b1083e67937
Sara-maria Arango-Franco <saf537@login-2-1.local>
2016-05-02 20:40:31 -0400

    Datetime and geopandas functionalities.

--- map.py
+++ map.py
@@ -4,28 +4,32 @@ import sys
 import datetime
 import geopandas as gp
 
-zipcodes = gp.read_file('NYCzipcodeshapefile2.geojson') # ??? How should we import this best?
+zipcodes = gp.read_file('/home/saf537/BigDataTaxisNYC/NYCzipcodeshapefile2.geojson') # ??? How should we import this best?
 
 for line in sys.stdin:
 	l = line.strip().split(',') #header condition
 	if len(l) == 19: #Figure this out
-		# taxis
-		pt_origin = gp.geoseries.Point(l.lon_origin,l.lat_origin) # column order
-		for x,z in enumerate(zipcodes['geometry']):
-    			if pt.intersects(z):
-        			zip_origin = zipcodes['postalCode']
-				break
-		pt_destin = gp.geoseries.Point(l.lon_destin,l.lat_destin) # column order
-		for x,z in enumerate(zipcodes['geometry']):
-    			if pt.intersects(z):
-        			zip_destin = zipcodes['postalCode']
-				break
-		try l.start_time - l.end_time: #Good condition here # column order
-			print "%s\t%d"% (str(zip_origin)+str(zip_destin),l.start_time - l.end_time) #Very careful, formatting. # column order
-	elif len(l)==15: #Figure this out
-		#bicycles
-		try l.start_time - l.end_time: #Good condition here
-			print "%s\t%d"% (str(zip_origin)+str(zip_destin),l.start_time - l.end_time) #Very careful, formatting.
+#		# taxis
+		# pickup_time = 1, dropoff_time = 2, dropoff_longitude = 10, dropoff_latitude = 11, pickup_longitude = 5, pickup_latitude = 64
+		pt_origin = gp.geoseries.Point(float(l[10],float(l[11])) # column order
+		pickup_time = datetime.datetime.strptime(l[1],"%Y-%m-%d %H:%M:%S")
+		dropoff_time = datetime.datetime.strptime(l[2],"%Y-%m-%d %H:%M:%S")
+
+#		for x,z in enumerate(zipcodes['geometry']):
+#    			if pt.intersects(z):
+#        			zip_origin = zipcodes['postalCode']1
+#				break
+#		pt_destin = gp.geoseries.Point(l.lon_destin,l.lat_destin) # column order
+#		for x,z in enumerate(zipcodes['geometry']):
+#    			if pt.intersects(z):
+#        			zip_destin = zipcodes['postalCode']
+#				break
+#		try l.start_time - l.end_time: #Good condition here # column order
+#			print "%s\t%d"% (str(zip_origin)+str(zip_destin),l.start_time - l.end_time) #Very careful, formatting. # column order
+#	elif len(l)==15: #Figure this out
+#		#bicycles
+#		try l.start_time - l.end_time: #Good condition here
+#			print "%s\t%d"% (str(zip_origin)+str(zip_destin),l.start_time - l.end_time) #Very careful, formatting.
 
 """
 
